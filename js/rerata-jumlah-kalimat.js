ee667da9805bb5b5a380659e1e9dac5427a215f9
unknown <222112305@stis.ac.id>
2023-12-13 15:39:42 +0700

    rata2

---
+++ js/rerata-jumlah-kalimat.js
@@ -0,0 +1,34 @@
+const config22 = {
+    type: 'bar',
+    data: {
+        labels: ['Attractions', 'Amenities', 'Access', 'Price', 'No Aspect'],
+        datasets: [{
+            label: '# of Votes',
+            data: [3.5492, 4.2459, 3.2434, 3.3409, 2.9451],
+            backgroundColor: ['lightskyblue', 'lightsalmon', 'lightgreen', 'lightcoral', 'mediumpurple'],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            x: {
+                title: {
+                    display: true,
+                    text: 'Aspek'
+                }
+            },
+            y: {
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'Total Jumlah Review'
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false,
+            }
+        }
+    }
+};
---
+++ js/top-10.js
@@ -0,0 +1,179 @@
+const config23 = {
+    type: 'bar',
+    data: {
+        labels: ['pantai', 'losari', 'makassar', 'indah', 'nikmat', 'bagus', 'sunset', 'pandang', 'kota', 'yg'],
+        datasets: [{
+            label: '# of Votes',
+            data: [2897, 1301, 1098, 977, 819, 782, 761, 632, 618, 607],
+            backgroundColor: ['lightskyblue', 'lightsalmon', 'lightgreen', 'lightcoral', 'mediumpurple'],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        indexAxis:'y',
+        scales: {
+            x: {
+                title: {
+                    display: true,
+                    text: 'Aspek'
+                }
+            },
+            y: {
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'Total Jumlah Review'
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false,
+            }
+        }
+    }
+};
+
+const config24 = {
+    type: 'bar',
+    data: {
+        labels: ['pantai', 'losari', 'makan', 'makassar', 'pisang', 'yg', 'jual', 'khas', 'bersih', 'sampah'],
+        datasets: [{
+            label: '# of Votes',
+            data: [1770, 851, 724, 713, 641, 516, 489, 429, 428, 381],
+            backgroundColor: ['lightskyblue', 'lightsalmon', 'lightgreen', 'lightcoral', 'mediumpurple'],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        indexAxis:'y',
+        scales: {
+            x: {
+                title: {
+                    display: true,
+                    text: 'Aspek'
+                }
+            },
+            y: {
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'Total Jumlah Review'
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false,
+            }
+        }
+    }
+};
+
+const config25 = {
+    type: 'bar',
+    data: {
+        labels: ['pantai', 'kota', 'losari', 'pusat', 'lokasi', 'makassar', 'jalan', 'mudah', 'letak', 'malam'],
+        datasets: [{
+            label: '# of Votes',
+            data: [289, 204, 147, 136, 129, 126, 114, 99, 57, 54],
+            backgroundColor: ['lightskyblue', 'lightsalmon', 'lightgreen', 'lightcoral', 'mediumpurple'],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        indexAxis:'y',
+        scales: {
+            x: {
+                title: {
+                    display: true,
+                    text: 'Aspek'
+                }
+            },
+            y: {
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'Total Jumlah Review'
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false,
+            }
+        }
+    }
+};
+
+const config26 = {
+    type: 'bar',
+    data: {
+        labels: ['pantai', 'harga', 'gratis', 'murah', 'parkir', 'pisang', 'yg', 'biaya', 'losari', 'masuk'],
+        datasets: [{
+            label: '# of Votes',
+            data: [84, 84, 59, 56, 53, 44, 39, 38, 36, 35],
+            backgroundColor: ['lightskyblue', 'lightsalmon', 'lightgreen', 'lightcoral', 'mediumpurple'],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        indexAxis:'y',
+        scales: {
+            x: {
+                title: {
+                    display: true,
+                    text: 'Aspek'
+                }
+            },
+            y: {
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'Total Jumlah Review'
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false,
+            }
+        }
+    }
+};
+
+const config27 = {
+    type: 'bar',
+    data: {
+        labels: ['pantai', 'makassar', 'losari', 'kunjung', 'kota', 'jalan', 'malam', 'yg', 'salah', 'wisata'],
+        datasets: [{
+            label: '# of Votes',
+            data: [2897, 1301, 1098, 977, 819, 782, 761, 632, 618, 607],
+            backgroundColor: ['lightskyblue', 'lightsalmon', 'lightgreen', 'lightcoral', 'mediumpurple'],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        indexAxis:'y',
+        scales: {
+            x: {
+                title: {
+                    display: true,
+                    text: 'Aspek'
+                }
+            },
+            y: {
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'Total Jumlah Review'
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false,
+            }
+        }
+    }
+};
